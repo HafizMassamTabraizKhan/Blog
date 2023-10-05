b1032bdd60defce4e1e526b2c808c82703afe386
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-10-06 02:59:56 +0500

    Add a callback method to update likes counter in like modle

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,9 @@ class Like < ApplicationRecord
   belongs_to :user, foreign_key: 'author', class_name: 'User'
   belongs_to :post, class_name: 'Post'
 
+  # Callback
+  after_save :update_likes_counter_for_post
+
   # Method
   def update_likes_counter_for_post
     post.increment!(:likes_counter)
