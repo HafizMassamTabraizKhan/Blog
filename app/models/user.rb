a06e7e34dffb27f93dd8e6ebd0915f77d9ec6f81
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-10-06 01:54:40 +0500

    Add required attributes in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
+
+  # Attributes
+  attribute :name, :string
+  attribute :photo, :string
+  attribute :bio, :text
+  attribute :posts_counter, :integer, default: 0
 end
