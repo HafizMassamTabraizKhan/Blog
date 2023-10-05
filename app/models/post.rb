1be1cdfc0286b7bc0b57566e0eeaecca54d72792
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-10-06 02:06:12 +0500

    Add required attributes in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,10 @@ class Post < ApplicationRecord
   belongs_to :author, calss_name: 'User'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  # Attributes
+  attribute :title, :string
+  attribute :text, :text
+  attribute :comments_counter, :integer, default: 0
+  attribute :likes_counter, :integer, default: 0
 end
