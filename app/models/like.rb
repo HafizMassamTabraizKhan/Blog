ca9c9a66251aea89121eb13845eae0c25c181249
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-10-06 02:58:44 +0500

    Add method to update likes counter in like modle

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,4 +2,9 @@ class Like < ApplicationRecord
   # Associations
   belongs_to :user, foreign_key: 'author', class_name: 'User'
   belongs_to :post, class_name: 'Post'
+
+  # Method
+  def update_likes_counter_for_post
+    post.increment!(:likes_counter)
+  end
 end
